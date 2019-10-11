bd1503eab76f36f465a318e94c5ac39e4fa6aa3c
Clinton Davelaar <DavelaarProgramming@gmail.com>
2019-10-11 16:01:07 -0500

    yep

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,17 +1,47 @@
-function sum(){
-	let i = parseInt(document.querySelector("#i").value);
-	const n = parseInt(document.querySelector("#to").value);
+function sum(i, n){
 	let result = 0;
 	for (i; i <= n; i++) {
 		result += i;
 	}
-	document.querySelector("#equals").value = result;
+	return result;
 }
 
 document.querySelector("#i").addEventListener("change", () => {
-	sum();
+	document.querySelector("#equals").value = sum(parseInt(document.querySelector("#i").value), parseInt(document.querySelector("#to").value));
+
+	localStorage.setItem("sum", sum(parseInt(document.querySelector("#i").value), parseInt(document.querySelector("#to").value)))
+
 });
 
 document.querySelector("#to").addEventListener("change", () => {
-	sum();
+	document.querySelector("#equals").value = sum(parseInt(document.querySelector("#i").value), parseInt(document.querySelector("#to").value));
+
+	localStorage.setItem("sum", sum(parseInt(document.querySelector("#i").value), parseInt(document.querySelector("#to").value)))
 });
+
+const jokeURI = 'https://icanhazdadjoke.com/slack'
+
+// fetch information
+const getJoke = async () => {
+  try {
+    const response = await fetch(jokeURI)
+    const obj = await response.json()
+    console.log(`FETCHED. Response JSON ${obj}`)
+    const joke = obj.attachments[0].fallback || 'No joke for you.'
+    return joke
+  } catch (error) { console.error(error) }
+}
+
+// interact with DOM
+const updateWithJoke = async (event) => {
+  try {
+    document.querySelector('#result').innerHTML = ''
+    const answer = await getJoke()
+    document.querySelector('#result').innerHTML = answer
+  } catch (error) { console.error(error) }
+}
+
+
+document.addEventListener('click', event => {
+  if (event.target && event.target.id === "ilikejokes") { updateWithJoke(event) }
+})
